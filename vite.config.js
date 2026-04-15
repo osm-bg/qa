a906b9275a825a142c35510ca0cf7a9df0a5586e
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2026-04-15 23:02:03 +0300

    do not put hash in filenames

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,15 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	build: {
+		rollupOptions: {
+			output: {
+				entryFileNames: 'assets/[name].js',
+				chunkFileNames: 'assets/[name].js',
+				assetFileNames: 'assets/[name][extname]'
+			}
+		}
+	},
 	server: {
 		fs: {
 			allow: [
