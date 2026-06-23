7a7bef993af5144eee1add70d5c332c912ea6054
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2026-06-23 15:25:19 +0300

    try to handle 404s on gh pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
-			fallback: undefined,
+			fallback: '404.html',
 			precompress: false,
 			strict: true,
 		}),
@@ -20,5 +18,3 @@ const config = {
 		}
 	}
 };
-
-export default config;
